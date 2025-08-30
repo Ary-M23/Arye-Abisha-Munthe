ca400236596b7aad8e4a5f7e301f179efcf1ef3d
Arye <aryemunthe26@gmail.com>
2025-08-31 02:47:50 +0700

    build: use CJS configs for tailwind/postcss in ESM project

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Arye-Abisha-Munthe/', // <— PENTING untuk GitHub Pages
+  base: '/Arye-Abisha-Munthe/', // case-sensitive
 });
